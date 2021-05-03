09c7431e9f1980a13b38d9fc5b5dfb9a7b275283
Masterchef365 <duncan.freeman1@gmail.com>
2021-05-02 18:03:28 -0700

    Add average tests

---
+++ average_tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import unittest
+from average import average
+
+class CubeTests(unittest.TestCase):
+    def test_math(self):
+        self.assertEqual(average([1., 2., 3.]), 2.)
+        self.assertEqual(average([99999., 2., 3.]), 33334.666666666664)
+
+    def test_type(self):
+        with self.assertRaises(TypeError):
+            average(5)
+        with self.assertRaises(TypeError):
+            average("Hello, world!")
+
+    def test_elem_type(self):
+        with self.assertRaises(TypeError):
+            average([1., 2., "Hello!"])
+        with self.assertRaises(TypeError):
+            average([7])
+
+    def test_empty_list(self):
+        with self.assertRaises(ValueError):
+            average([])
+
+if __name__ == '__main__':
+    unittest.main()
